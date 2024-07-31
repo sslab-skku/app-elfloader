eb9aca8656d80fcfac59bea14307652e902f83f2
Kha Dinh <khadinh@g.skku.edu>
2024-07-31 09:33:37 +0000

    build_image and makefile fix
    Signed-off-by: Kha Dinh <khadinh@g.skku.edu>

--- build_image.sh
+++ build_image.sh
@@ -13,11 +13,12 @@ DEFCONFIG=$(realpath ./sev_virtio)
 # UK_DEFCONFIG=$DEFCONFIG make defconfig
 . .config
 UK_APP=$CONFIG_UK_DEFNAME
-
 CMD_LINE=${UK_APP}.cmdl
 BINARY=build/${UK_APP}_qemu-x86_64
 
-DYN_APP=helloworld
+# DYN_APP=helloworld
+# DYN_APP=a.out
+DYN_APP=a.out
 echo "Building $UK_APP"
 
 # Update config accordingly
@@ -25,17 +26,25 @@ echo "Building $UK_APP"
 # Prepare cmdl
 rm -rf $CMD_LINE
 # if [ ! -f $CMD_LINE ]; then
-echo "$UK_APP netdev.ipv4_addr=172.44.0.2 netdev.ipv4_gw_addr=172.44.0.1 netdev.ipv4_subnet_mask=255.255.255.0 -- $DYN_APP" >$CMD_LINE
+# LD_DEBUG=all
+echo "$UK_APP env.vars=[ LD_LIBRARY_PATH=/lib/x86_64-linux-gnu/ LD_BIND_NOW=1 LD_DEBUG=all  ]  netdev.ipv4_addr=172.44.0.2 netdev.ipv4_gw_addr=172.44.0.1 netdev.ipv4_subnet_mask=255.255.255.0 -- $DYN_APP -cCOM.DAT" >$CMD_LINE
 # fi
 
 # Build initrd cpio file
 mkdir -p fs0
+
+cp -rf prebuilt/$DYN_APP/* fs0
+
 pushd fs0
 find -depth -print | tac | bsdcpio -o --format newc >../fs0.cpio
 popd
 
+. .config
+UK_APP=$CONFIG_UK_DEFNAME
+CMD_LINE=${UK_APP}.cmdl
 BINARY=build/${UK_APP}_qemu-x86_64
 # Finally, build the image
+#
 make -j32
 rm -rf $OUT_ISO
 sudo $UNIKRAFT_DIR/support/scripts/mkukimg -f iso -i fs0.cpio -k $BINARY -b ukefi -a X64 -c $CMD_LINE -o $OUT_ISO
